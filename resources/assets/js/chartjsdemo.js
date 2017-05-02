8bc99d4956c357811178c1feff363dabd43d5e68
Pedro Martinez <pmartinez1085@gmail.com>
2017-05-02 16:58:18 +0200

    sync

---
+++ resources/assets/js/chartjsdemo.js
@@ -0,0 +1,32 @@
+// Total ventas per més
+// $( document ).ready(function() {
+//
+//     var data = {
+//
+//         type: 'line',
+//         // x
+//         labels: ['Gener', 'Febrer', 'Març'],
+//
+//         // y
+//         datasets: [
+//             {
+//                 label: "Samarretes",
+//                 data: [25, 50, 5]
+//             },
+//             {
+//                 label: "Pantalons",
+//                 data: [50, 45, 30]
+//             }
+//         ]
+//     }
+//
+//     var context = document.querySelector('#graph').getContext('2d')
+//
+//     // new Chart(context,data)
+//     new Chart(context, {
+//         type: 'line',
+//         data: data
+//     });
+// });
+
+//new Chart(context).Line(data);
---
+++ resources/assets/js/components/Graph.vue
@@ -0,0 +1,30 @@
+<template>
+    <canvas id="graph"></canvas>
+</template>
+
+<script>
+    import Chart from 'chart.js'
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+            var data = {
+             labels: ['gener', 'febrer','març'],
+             datasets: [
+                 {
+                     label: 'Samarretes',
+                     data: [25,50,5]
+                 },
+                 {
+                     label: 'Pantalons',
+                     data: [50, 45, 30]
+                 }
+             ]
+            }
+            var context = document.querySelector('#graph').getContext('2d')
+            new Chart(context,{
+             type: "bar",
+             data: data,
+            })
+        }
+    }
+</script>
